575048d7d67c5be507455d6cd208bc12f6a11a06
Karen Flores <karen.ag.flores@gmail.com>
2018-07-03 09:05:48 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -2,21 +2,21 @@ require 'pry'
 class MP3Importer
   attr_accessor :path
 
-  def initialize(filepath)
-    @path = filepath
+  def initialize(path)
+    @path = path
   end
 
   def files
     file_list = Dir["#{@path}/*.mp3"]
-    @files = []
-    @files = file_list.each do |path|
-      path.slice!(0,21)
-    end
+    @files = file_list.each {|file| file.slice!(0,21)}
     @files
+    #binding.pry
   end
 
   def import
+    #binding.pry
     files.each {|file| Song.new_by_filename(file)}
+    #Song.new_by_filename(file)
   end
 
 end
