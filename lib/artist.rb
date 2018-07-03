61be5d7d43877b5058b586d159f96d23fa6ef02a
Karen Flores <karen.ag.flores@gmail.com>
2018-07-03 09:05:01 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,7 @@
-require 'pry'
 class Artist
   attr_accessor :name, :songs
   @@all = []
+
 
   def initialize(name)
     @name = name
@@ -11,33 +11,33 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
+    #@songs << song
   end
 
-  #def self.all=(all)
-  #  @@all = all
-  #end
-
-  def self.find_or_create_by_name(name)
-    if self.all.find{|artist| artist.name == name}
-    else
-      self.new(name).tap {|artist| artist.save}
-    end
-  end
-
-  #def artist_name=(name)
-  #  self.artist = Artist.find_or_create_by_name(name)
-  #end
-
-  def self.all
-    @@all
+  def songs
+    @songs
   end
 
   def save
     @@all << self
   end
 
+  def self.find_or_create_by_name(name)
+    if Artist.all.find {|artist| artist.name == name}
+      performer = Artist.name
+    else
+      performer = Artist.new(name)
+      #@@all << performer
+      #performer.save
+    end
+  end
+
+  def self.all
+    @@all
+  end
+
   def print_songs
-    #binding.pry
     songs.each {|song| puts song.name}
   end
+
 end
